3aaddea772169a39bc94ad8f110346f2075ab676
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-16 17:58:32 -0300

    chore: jest config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  roots: ['<rootDir>/backend/src', '<rootDir>/frontend/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/backend/src/**/*.ts', '<rootDir>/frontend/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1',
+  },
+}
