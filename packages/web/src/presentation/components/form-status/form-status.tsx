8b8c0828be545c35c0fc55538b2268660ff17c08
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-21 16:08:54 -0300

    feat: ensure Login do not render form status message on start

--- packages/web/src/presentation/components/form-status/form-status.tsx
+++ packages/web/src/presentation/components/form-status/form-status.tsx
@@ -7,14 +7,14 @@ type Props = {
 
 const FormStatus: React.FC<Props> = (props: Props) => {
   return (
-    <>
+    <div data-testid='status-wrap'>
       {props.error && (
         <div className='badge badge-pill badge-danger border p-2 my-2 w-100'>{props.error}</div>
       )}
       {props.success && (
         <div className='badge badge-pill badge-success border p-2 my-2 w-100'>{props.success}</div>
       )}
-    </>
+    </div>
   )
 }
 
--- packages/web/src/presentation/pages/login/login.tsx
+++ packages/web/src/presentation/pages/login/login.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 import { CustomCheckbox, FormStatus, Input } from '@/presentation/components'
+import FormContext from '@/presentation/contexts/form/form-context'
 import LoginAside from './login-aside'
 
+type StateProps = {
+  errorMessage?: string
+  successMessage?: string
+}
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({})
   const [email, setEmail] = useState('demo@example.com')
   const inputMessages = { email: '', password: '' }
   const [messages] = useState<any>(inputMessages)
@@ -22,40 +29,48 @@ const Login: React.FC = () => {
             <div className='row no-gutters'>
               <div className='col-xl-12 tab-content'>
                 <div id='sign-in' className={`auth-form form-validation`}>
-                  <FormStatus error={messages.error} success={messages.success}></FormStatus>
-                  <form onSubmit={onLogin} className='form-validate'>
-                    <h3 className='text-center mb-4 text-black'>Sign in your account</h3>
-                    <Input
-                      id='email'
-                      label='Email'
-                      type='email'
-                      value={email}
-                      placeholder='Type Your Email Address'
-                      onChange={e => setEmail(e.target.value)}
-                      errorMessage={messages.email}
-                    ></Input>
-                    <Input
-                      label='Password'
-                      type='password'
-                      value={password}
-                      placeholder='Type Your Password'
-                      onChange={e => setPassword(e.target.value)}
-                      errorMessage={messages.password}
-                    ></Input>
-                    <div className='form-row d-flex justify-content-between mt-4 mb-2'>
-                      <CustomCheckbox id='remember' label='Remember my preference'></CustomCheckbox>
+                  <FormContext.Provider value={state}>
+                    <FormStatus
+                      error={state.errorMessage}
+                      success={state.successMessage}
+                    ></FormStatus>
+                    <form onSubmit={onLogin} className='form-validate'>
+                      <h3 className='text-center mb-4 text-black'>Sign in your account</h3>
+                      <Input
+                        id='email'
+                        label='Email'
+                        type='email'
+                        value={email}
+                        placeholder='Type Your Email Address'
+                        onChange={e => setEmail(e.target.value)}
+                        errorMessage={messages.email}
+                      ></Input>
+                      <Input
+                        label='Password'
+                        type='password'
+                        value={password}
+                        placeholder='Type Your Password'
+                        onChange={e => setPassword(e.target.value)}
+                        errorMessage={messages.password}
+                      ></Input>
+                      <div className='form-row d-flex justify-content-between mt-4 mb-2'>
+                        <CustomCheckbox
+                          id='remember'
+                          label='Remember my preference'
+                        ></CustomCheckbox>
+                      </div>
+                      <div className='text-center form-group mb-3'>
+                        <button type='submit' className='btn btn-primary btn-block'>
+                          Sign In
+                        </button>
+                      </div>
+                    </form>
+                    <div className='new-account mt-3'>
+                      <p>
+                        {"Don't have an account?"} <a className='text-primary'>Sign up</a>
+                      </p>
                     </div>
-                    <div className='text-center form-group mb-3'>
-                      <button type='submit' className='btn btn-primary btn-block'>
-                        Sign In
-                      </button>
-                    </div>
-                  </form>
-                  <div className='new-account mt-3'>
-                    <p>
-                      {"Don't have an account?"} <a className='text-primary'>Sign up</a>
-                    </p>
-                  </div>
+                  </FormContext.Provider>
                 </div>
               </div>
             </div>
