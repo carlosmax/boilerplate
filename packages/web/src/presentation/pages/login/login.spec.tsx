8464c293ddfd7444ca6c7f1262fefe22e6f66b5b
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-21 20:04:06 -0300

    test: ensure Login starts with correct form state

--- packages/web/src/presentation/pages/login/login.spec.tsx
+++ packages/web/src/presentation/pages/login/login.spec.tsx
@@ -4,9 +4,13 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
-  test('Should not render form status message on start', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(<Login></Login>)
     const statusWrap = getByTestId('status-wrap')
     expect(statusWrap.childElementCount).toBe(0)
+    const emailError = getByTestId('email-error')
+    expect(emailError.textContent).toBe('')
+    const passwordError = getByTestId('password-error')
+    expect(passwordError.textContent).toBe('')
   })
 })
