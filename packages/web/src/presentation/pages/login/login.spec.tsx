7aeee6c168ec2df7b8a5962b8afd6061ec56378d
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-21 20:09:57 -0300

    refactor: move sut creation to a factory method

--- packages/web/src/presentation/pages/login/login.spec.tsx
+++ packages/web/src/presentation/pages/login/login.spec.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 
 import Login from './login'
 
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
+
 describe('Login Component', () => {
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Login></Login>)
-    const statusWrap = getByTestId('status-wrap')
+    const { sut } = makeSut()
+    const statusWrap = sut.getByTestId('status-wrap')
     expect(statusWrap.childElementCount).toBe(0)
-    const emailError = getByTestId('email-error')
+    const emailError = sut.getByTestId('email-error')
     expect(emailError.textContent).toBe('')
-    const passwordError = getByTestId('password-error')
+    const passwordError = sut.getByTestId('password-error')
     expect(passwordError.textContent).toBe('')
   })
 })
