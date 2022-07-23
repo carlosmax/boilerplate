22031d011884f8c27be2e853dca8a0f70f066e88
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-23 20:47:27 -0300

    refactor: move makeLoginValidation to a separate file

--- packages/web/src/main/factories/pages/login-factory.tsx
+++ packages/web/src/main/factories/pages/login-factory.tsx
@@ -1,14 +1,8 @@
 import React, { ReactElement } from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases'
-import { ValidationComposite, ValidationBuilder as Builder } from '@monorepo/validation'
+import { makeLoginValidation } from '../validations'
 
 export const makeLogin = (): ReactElement<any, any> => {
-  const makeLoginValidation = (): ValidationComposite =>
-    ValidationComposite.build([
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build()
-    ])
-
   return <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />
 }
