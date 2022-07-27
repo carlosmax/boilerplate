87560bee4de0dcfe0668b16ea535ff1a9257a9ac
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-27 13:26:42 -0300

    test: ensure Login shows error state if form is invalid

--- packages/web/cypress/e2e/login.spec.cy.ts
+++ packages/web/cypress/e2e/login.spec.cy.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -6,6 +8,15 @@ describe('Login', () => {
   it('Should load with correct initial state', () => {
     cy.getByTestId('email-error').should('be.empty')
     cy.getByTestId('password-error').should('be.empty')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('status-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('email-error').should('contain.text', 'Endereço de email inválido')
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-error').should('contain.text', 'Valor inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('status-wrap').should('not.have.descendants')
   })
