377e00a26985d9bff23d92cfca5bfaab22f40b12
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-27 13:15:24 -0300

    test: ensure Login starts with correct initial state

--- packages/web/cypress/e2e/login.spec.cy.ts
+++ packages/web/cypress/e2e/login.spec.cy.ts
@@ -1,5 +1,12 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-error').should('be.empty')
+    cy.getByTestId('password-error').should('be.empty')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('status-wrap').should('not.have.descendants')
   })
 })
