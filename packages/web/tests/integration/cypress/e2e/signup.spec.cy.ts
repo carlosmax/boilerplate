3c61af7c68a4a64449b57fd4b351c8362a3ba3c2
Carlos Max <carlosmax.dev@hotmail.com>
2022-07-29 17:36:04 -0300

    test: ensure Signup resets state on page load

--- packages/web/tests/integration/cypress/e2e/signup.spec.cy.ts
+++ packages/web/tests/integration/cypress/e2e/signup.spec.cy.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('SignUp', () => {
   beforeEach(() => {
     cy.visit('signup')
@@ -10,5 +12,16 @@ describe('SignUp', () => {
     cy.getByTestId('passwordConfirmation-error').should('be.empty')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('status-wrap').should('not.have.descendants')
+  })
+
+  it('Should reset state on page load', () => {
+    cy.getByTestId('name').type(faker.name.findName())
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('login-link').click()
+    cy.getByTestId('signup-link').click()
+    cy.getByTestId('name').should('be.empty')
+    cy.getByTestId('name-error').should('be.empty')
+    cy.getByTestId('email').should('be.empty')
+    cy.getByTestId('email-error').should('be.empty')
   })
 })
