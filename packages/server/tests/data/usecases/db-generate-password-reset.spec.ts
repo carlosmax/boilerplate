c6ff9bb7c0d65b1b470c109cae3c821b7ee79d26
Carlos Max <carlosmax.dev@hotmail.com>
2022-08-27 20:51:05 -0300

    test: ensure DbGeneratePasswordReset throws if LoadAccountByEmailRepository throws

--- packages/server/tests/data/usecases/db-generate-password-reset.spec.ts
+++ packages/server/tests/data/usecases/db-generate-password-reset.spec.ts
@@ -1,5 +1,7 @@
 import { DbGeneratePasswordReset } from '@/data/usecases'
+import { GeneratePasswordReset } from '@/domain/usecases'
 import { faker } from '@faker-js/faker'
+import { throwError } from '../../domain/mocks'
 import { LoadAccountByEmailRepositorySpy } from '../mocks'
 
 type SutTypes = {
@@ -16,11 +18,22 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const mockGeneratePasswordResetParams = (): GeneratePasswordReset.Params => {
+  return { email: faker.internet.email() }
+}
+
 describe('DbGeneratePasswordReset UseCase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
     const { sut, loadAccountByEmailRepositorySpy } = makeSut()
-    const params = { email: faker.internet.email() }
+    const params = mockGeneratePasswordResetParams()
     await sut.generate(params)
     expect(loadAccountByEmailRepositorySpy.email).toBe(params.email)
+  })
+
+  test('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositorySpy } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositorySpy, 'loadByEmail').mockImplementationOnce(throwError)
+    const promise = sut.generate(mockGeneratePasswordResetParams())
+    await expect(promise).rejects.toThrow()
   })
 })
