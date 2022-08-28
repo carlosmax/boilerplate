3a038fc358035540c233e0092f1c3658d3681e49
Carlos Max <carlosmax.dev@hotmail.com>
2022-08-28 11:20:54 -0300

    refactor: get client url from env

--- packages/server/src/main/config/env.ts
+++ packages/server/src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'tj67O==7H'
+  jwtSecret: process.env.JWT_SECRET || 'tj67O==7H',
+  clientUrl: process.env.CLIENT_URL || 'http://localhost:3000'
 }
--- packages/server/src/main/config/middlewares.ts
+++ packages/server/src/main/config/middlewares.ts
@@ -3,12 +3,13 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 
 import express, { Express } from 'express'
+import env from '@/main/config/env'
 // import { Logger } from '@monorepo/logger'
 
 // const LOGGER = Logger('ROUTE')
 
 export default (app: Express): void => {
-  app.use(cors({ origin: process.env.CLIENT_URL }))
+  app.use(cors({ origin: env.clientUrl }))
   app.use(helmet())
   app.use(express.json())
 
