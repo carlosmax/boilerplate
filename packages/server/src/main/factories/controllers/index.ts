3b91fe01638e9dee389fcee2c6de7aaa2b4cc8db
Carlos Max <carlosmax.dev@hotmail.com>
2022-08-30 19:46:59 -0300

    feat: add request reset password route

--- packages/server/src/main/factories/controllers/index.ts
+++ packages/server/src/main/factories/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './signup-controller-factory'
 export * from './login-controller-factory'
+export * from './generate-password-reset-controller-factory'
--- packages/server/src/main/routes/auth-routes.ts
+++ packages/server/src/main/routes/auth-routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 
 import { adaptRoute } from '@/main/adapters'
-import { makeLoginController, makeSignUpController } from '@/main/factories'
+import {
+  makeLoginController,
+  makeSignUpController,
+  makeGeneratePasswordResetController
+} from '@/main/factories'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
   router.post('/login', adaptRoute(makeLoginController()))
+  router.post('/request-reset-password', adaptRoute(makeGeneratePasswordResetController()))
 }
