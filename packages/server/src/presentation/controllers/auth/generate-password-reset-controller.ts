e5ba9f4c46894622414fc2a1a94420ea301ad6f0
Carlos Max <carlosmax.dev@hotmail.com>
2022-08-30 18:36:03 -0300

    feat: ensure GeneratePasswordResetController calls Validation with correct value

--- packages/server/src/presentation/controllers/auth/generate-password-reset-controller.ts
+++ packages/server/src/presentation/controllers/auth/generate-password-reset-controller.ts
@@ -1,12 +1,17 @@
 import { GeneratePasswordReset } from '@/domain/usecases'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { noContent, serverError } from '@/presentation/helpers'
+import { ValidationInput } from '@monorepo/validation'
 
 export class GeneratePasswordResetController implements Controller {
-  constructor(private readonly generatePasswordReset: GeneratePasswordReset) {}
+  constructor(
+    private readonly generatePasswordReset: GeneratePasswordReset,
+    private readonly validation: ValidationInput
+  ) {}
 
   async handle(request: GeneratePasswordResetController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validateInput(request)
       await this.generatePasswordReset.generate(request)
       return noContent()
     } catch (error) {
