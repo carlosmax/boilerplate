6fe646489e71e19ccac7f98eaaebddd96580374b
Carlos Max <carlosmax.dev@hotmail.com>
2022-09-05 22:24:06 -0300

    refactor: ensure DbPasswordReset throws if LoadAccountByIdRepository returns null

--- packages/server/src/data/usecases/db-password-reset.ts
+++ packages/server/src/data/usecases/db-password-reset.ts
@@ -7,11 +7,7 @@ export class DbPasswordReset implements PasswordReset {
   async reset(params: PasswordReset.Params): Promise<boolean> {
     const account = await this.loadAccountByIdRepository.loadById(params.accountId)
 
-    if (!account) {
-      return false
-    }
-
-    if (!account.resetPasswordToken) {
+    if (!account || !account.resetPasswordToken) {
       throw new Error('Token de redefinição de senha inválido ou expirado!')
     }
 
